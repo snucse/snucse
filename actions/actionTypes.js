58c51c7d2d6ff8cceb59778c7478e33ff8381c13
Leeingnyo <dyd1928@naver.com>
2016-09-17 18:03:37 +0900

    add action types

---
+++ actions/actionTypes.js
@@ -0,0 +1,5 @@
+
+// action types related with Post.js
+export const LOAD_POST = 'LOAD_POST'
+export const LOAD_INITIAL_POST = 'LOAD_INITIAL_POST'
+export const SCROLL_POST_LIST_END = 'SCROLL_POST_LIST_END'
