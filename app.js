d5a2ce4e69d43796643592a464d1272131a6431f
Leeingnyo <dyd1928@naver.com>
2016-09-18 23:32:49 +0900

    앱에 스토어 반영
    Provider store = {store} 를 하게 되면
    connect 함수를 사용할 때 store가 등록되어져서
    state와 dispatch를 사용할 수 있게 된다

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import $ from 'jquery';
 import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router';
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
 
 import Menu from './Menu.js';
 import Others from './Others.js';
@@ -13,6 +15,8 @@ import Post_Write from './Post_Write.js';
 import Edit from './Post_Edit.js';
 import Post from './Post.js';
 import ClassManager from './ClassManager.js';
+
+import reducers from './reducers'
 
 const rootElement = document.getElementById('content');
 
@@ -29,16 +33,20 @@ var Test = React.createClass({
 var url = "http://snucse.snucse.org:32123/api/v1/";
 //var url = "http://aws.izz.kr:3000/api/v1/";
 
+const store = createStore(combineReducers(reducers))
+
 ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route path="/" component={Menu} pollInterval={2000} url={url}>
-      <IndexRoute component={Main} url={url+"articles"} pollInterval={2000} />
-      <Route path="/message" component={Message} />
-      <Route path="/others" component={Others} />
-      <Route path="/:post_id/edit" component={Edit} url={url+"articles"} />
-      <Route path="/groups" component={Group} url={url+"profiles"} />
-      <Route path="/:id" component={ClassManager} url={url} />
-      <Route path="/group/:id/write" component={Post_Write} url={url+"articles"} />
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={Menu} pollInterval={2000} url={url}>
+        <IndexRoute component={Main} url={url+"articles"} pollInterval={2000} />
+        <Route path="/message" component={Message} />
+        <Route path="/others" component={Others} />
+        <Route path="/:post_id/edit" component={Edit} url={url+"articles"} />
+        <Route path="/groups" component={Group} url={url+"profiles"} />
+        <Route path="/:id" component={ClassManager} url={url} />
+        <Route path="/group/:id/write" component={Post_Write} url={url+"articles"} />
+      </Route>
+    </Router>
+  </Provider>
   , rootElement);
