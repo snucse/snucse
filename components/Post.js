caf326945d99d129e3c27d5fdc578a729f464b6a
JW275 <2jwdream7029@naver.com>
2016-09-24 17:00:38 +0900

    scroll 이벤트 핸들러를 render 밖으로 빼냄
    탈 jQuery

--- components/Post.js
+++ components/Post.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import $ from 'jquery';
 import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router';
 import { connect } from 'react-redux';
 import CommentBox from './Comment.js';
@@ -23,34 +22,41 @@ var Post = React.createClass({
     DataCon.loadDataFromServer(url, success);
   },
 
+  onScroll() {
+    const list = document.documentElement;
+    // http://stackoverflow.com/questions/9439725
+    if(window.innerHeight + window.scrollY >= document.body.scrollHeight) {
+      if (this.state.loading === true) {
+        return;
+      } else {
+        setTimeout(() => {
+          if (this.props.data.articles.length > this.props.post_num) {
+            // 보여주는 것보다 갖고 있는게 더 적으면
+            this.props.onScrollEnd()
+            // 더 보여달라는 요청
+          }
+          this.setState({loading: false});
+        }, 1000);
+        this.setState({loading: true});
+        return;
+      }
+    }
+  },
+
   componentDidMount: function() {
+    window.addEventListener('scroll', this.onScroll);
     this.loadPostFromServer();
   },
 
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.onScroll);
+  },
+
   getInitialState: function() {
-    return {}
+    return {loading: false}
   },
 
   render: function() {
-    $(window).scroll(() => {
-      var loading = false
-      if($(window).scrollTop() == $(document).height() - $(window).height()) {
-        if (loading === true) {
-          return;
-        } else {
-          setTimeout(() => {
-            if (this.props.data.articles.length > this.props.post_num) {
-              // 보여주는 것보다 갖고 있는게 더 적으면
-              this.props.onScrollEnd()
-              // 더 보여달라는 요청
-            }
-            loading = false;
-          }, 1000);
-          loading = true;
-          return;
-        }
-      }
-    });
     var flag = 0;
     var count = 0;
     var postNodes = this.props.data.articles.map((post) => {
