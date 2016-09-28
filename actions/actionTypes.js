8e8798c91f6e277edabffc0bbed0a7e7d4dc08f8
Leeingnyo <dyd1928@naver.com>
2016-09-29 01:10:45 +0900

    add actions

--- actions/actionTypes.js
+++ actions/actionTypes.js
@@ -3,3 +3,9 @@
 export const LOAD_POST = 'LOAD_POST'
 export const LOAD_INITIAL_POST = 'LOAD_INITIAL_POST'
 export const SCROLL_POST_LIST_END = 'SCROLL_POST_LIST_END'
+
+// action types related with comment
+export const LOAD_COMMENT = 'LOAD_COMMENT'
+export const WRITE_COMMENT = 'WRITE_COMMENT'
+export const EDIT_COMMENT = 'EDIT_COMMENT'
+export const DELETE_COMMENT = 'DELETE_COMMENT'
--- actions/index.js
+++ actions/index.js
@@ -20,3 +20,36 @@ export function scrollPostListEnd(){
     type: types.SCROLL_POST_LIST_END,
   }
 }
+
+// action related with comment
+export function loadComment(articleId, comments){
+  return {
+    type: types.LOAD_COMMENT,
+    comments: comments,
+    articleId: articleId,
+  }
+}
+
+export function writeComment(articleId, comment){
+  return {
+    type: types.WRITE_COMMENT,
+    comment: comment,
+    articleId: articleId,
+  }
+}
+
+export function editComment(articleId, comment){
+  return {
+    type: types.EDIT_COMMENT,
+    comment: comment,
+    articleId: articleId,
+  }
+}
+
+export function deleteComment(articleId, comment){
+  return {
+    type: types.DELETE_COMMENT,
+    comment: comment,
+    articleId: articleId,
+  }
+}
