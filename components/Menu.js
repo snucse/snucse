ebbfff7359f12725ba88fd6507bcbfeb8db78c65
JW275 <2jwdream7029@naver.com>
2016-10-08 19:39:35 +0900

    빈줄 저리갓

--- components/Menu.js
+++ components/Menu.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router';
 import {DataCon, Url} from '../utils';
-
 import {loadUserId} from '../actions';
 import MyProfile from './MyProfile.js';
 
