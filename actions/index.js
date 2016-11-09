c67352d3960c9d08c44af60d9171331fd2da184f
Sanghyeok Park <pps0209@gmail.com>
2016-11-09 11:27:00 +0900

    실수로 지운 부분 복구

--- actions/index.js
+++ actions/index.js
@@ -1,5 +1,26 @@
 import * as types from './actionTypes';
 
+// action related with Article component
+export function loadInitialArticle(data) {
+  return {
+    type: types.LOAD_INITIAL_ARTICLE,
+    data
+  };
+}
+
+export function loadArticle(data) {
+  return {
+    type: types.LOAD_ARTICLE,
+    data
+  };
+}
+
+export function scrollArticleListEnd() {
+  return {
+    type: types.SCROLL_ARTICLE_LIST_END
+  };
+}
+
 export function onLoadArticle() {
   return {
     type: types.ON_LOADING_ARTICLE
