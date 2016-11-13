39f0a8e16b0ea99bc29c36b3165f97f98ac03956
Leeingnyo <dyd1928@naver.com>
2016-11-13 22:57:02 +0900

    implement candidate tag form

--- components/TagBox/TagForm.js
+++ components/TagBox/TagForm.js
@@ -3,12 +3,82 @@ import React from 'react';
 import {genRefCallback} from '../../utils';
 
 const TagForm = React.createClass({
-  handleClickAdd() {
+  addTag() {
     if ((this._content || false) && this._content.value !== '') {
       this.props.onAdd(this._content.value);
       this._content.value = '';
-      this.setState({isEditMode: false});
+      this.setState({
+        isEditMode: false,
+        isShowCandidateTags: false,
+        candidateTag: -1
+      });
     }
+  },
+
+  handleClickAdd() {
+    this.addTag();
+  },
+
+  loadCandidateTagsTimeout: null,
+
+  handleChangeInput(event) {
+    if (this._content.value === '') {
+      this.setState({isShowCandidateTags: false, candidateTag: -1});
+      return;
+    }
+    const query = event.target.value;
+    clearTimeout(this.loadCandidateTagsTimeout);
+    this.loadCandidateTagsTimeout = setTimeout(() => {
+      // load auto complete tag list
+      this.props.onLoadCandidateTags(query);
+      this.setState({isShowCandidateTags: true, candidateTag: -1});
+    }, 300);
+  },
+
+  handleKeyDownInput(event) {
+    if (event.keyCode == 13) {
+      this.addTag();
+    } else if (event.keyCode === 40) {
+      const candidates = this.props.candidateTags || [];
+      if (candidates.length === 0) {
+        return;
+      }
+      // down
+      if (candidates.length > this.state.candidateTag + 1) {
+        this.setState({
+          isShowCandidateTags: true,
+          candidateTag: this.state.candidateTag + 1
+        });
+      } else {
+        this.setState({
+          isShowCandidateTags: true,
+          candidateTag: 0
+        });
+      }
+    } else if (event.keyCode === 38) {
+      const candidates = this.props.candidateTags || [];
+      if (candidates.length === 0) {
+        return;
+      }
+      // up
+      if (this.state.candidateTag > 0) {
+        this.setState({
+          isShowCandidateTags: true,
+          candidateTag: this.state.candidateTag - 1
+        });
+      } else {
+        this.setState({
+          isShowCandidateTags: true,
+          candidateTag: candidates.length - 1
+        });
+      }
+    }
+  },
+
+  handleClickCandidateTag(event) {
+    this._content.value = event.currentTarget.innerHTML;
+    // fixme search instead of innerHTML...
+    this.setState({isShowCandidateTags: false, candidateTag: -1});
   },
 
   handleClickShowForm() {
@@ -21,16 +91,44 @@ const TagForm = React.createClass({
 
   getInitialState() {
     return {
-      isEditMode: false
+      isEditMode: false,
+      isShowCandidateTags: false,
+      candidateTag: -1
     };
   },
 
   render() {
+    const candidateTags = this.props.candidateTags || [];
+    const candidateTagListItems = candidateTags.map((candidateTag, i) => {
+      let clazz;
+      if (i === this.state.candidateTag) {
+        clazz = 'autocomplete-tag-item selected';
+        if (this._content !== null) {
+          this._content.value = candidateTag.tag;
+        }
+      } else {
+        clazz = 'autocomplete-tag-item';
+      }
+      return (
+        <li className={clazz} onClick={this.handleClickCandidateTag} key={candidateTag.tag}>
+          {candidateTag.tag}
+        </li>
+      );
+    });
+    const candidateTagList = this.state.isShowCandidateTags ?
+      <ul className="autocomplete-tag-list">
+        {candidateTagListItems}
+      </ul> : null;
     const form = this.state.isEditMode ?
       <form className="tag-form">
-        <input ref={genRefCallback(this, '_content')}/>
+        <input
+          ref={genRefCallback(this, '_content')}
+          onChange={this.handleChangeInput}
+          onKeyDown={this.handleKeyDownInput}
+          />
         <input onClick={this.handleClickAdd} type="button" value="추가"/>
         <input onClick={this.handleClickHideForm} type="button" value="취소"/>
+        {candidateTagList}
       </form> :
       null;
     return (
