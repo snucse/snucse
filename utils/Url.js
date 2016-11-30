c47f11ee3866fc010037d0982a602ce58780fcd1
Leeingnyo <dyd1928@naver.com>
2016-11-30 12:46:10 +0900

    fix bug? improve getUrl

--- utils/Url.js
+++ utils/Url.js
@@ -3,7 +3,9 @@ export default {
     let query = '?';
     if (typeof params === 'object') {
       for (const key of Object.keys(params)) {
-        query += `${key}=${params[key]}`;
+        if (params[key] !== undefined) {
+          query += `${key}=${params[key]}`;
+        }
       }
     }
     return `/api/v1${path}${query}`;
