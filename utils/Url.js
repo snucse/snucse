105b44c66941afa7f9073c562d732837994817d5
Leeingnyo <dyd1928@naver.com>
2016-11-30 10:15:04 +0900

    apply review

--- utils/Url.js
+++ utils/Url.js
@@ -2,10 +2,8 @@ export default {
   getUrl(path, params = {}) {
     let query = '?';
     if (typeof params === 'object') {
-      for (const key in params) {
-        if ({}.hasOwnProperty.call(params, key)) {
-          query += `${key}=${params[key]}`;
-        }
+      for (const key of Object.keys(params)) {
+        query += `${key}=${params[key]}`;
       }
     }
     return `/api/v1${path}${query}`;
