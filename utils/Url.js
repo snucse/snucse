37c8c8b38335e7a3c5e5176914400cbb5e35d48d
Leeingnyo <dyd1928@naver.com>
2016-11-30 23:05:48 +0900

    hotfix: fix getUrl, add '&' and refactoring

--- utils/Url.js
+++ utils/Url.js
@@ -1,13 +1,14 @@
 export default {
   getUrl(path, params = {}) {
-    let query = '?';
+    const query = [];
     if (typeof params === 'object') {
       for (const key of Object.keys(params)) {
         if (params[key] !== undefined) {
-          query += `${key}=${params[key]}`;
+          query.push(`${key}=${params[key]}`);
         }
       }
     }
-    return `/api/v1${path}${query}`;
+    const queryString = query.length === 0 ? '' : `?${query.join('&')}`;
+    return `/api/v1${path}${queryString}`;
   }
 };
