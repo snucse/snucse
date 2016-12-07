2fb13364688deeab962dcac0c65887ea5e4d8063
Sanghyeok Park <pps0209@gmail.com>
2016-12-07 11:40:04 +0900

    remove method utils.UserLevel.*

--- utils/UserLevel.js
+++ utils/UserLevel.js
@@ -1,11 +1,6 @@
 const UserLevel = {
   ASSOCIATE: 1,
   REGULAR: 2,
-
-  getRenderer(mapUserLevelToRenderer, userLevel) {
-    return mapUserLevelToRenderer[userLevel] ||
-      mapUserLevelToRenderer.default;
-  }
 };
 
 export default UserLevel;
