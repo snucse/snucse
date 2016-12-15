a63f15e2bfaa8fb34dc880c0d48df954e5b208ce
Sanghyeok Park <pps0209@gmail.com>
2016-12-15 10:58:56 +0900

    글 목록(또는 피드)에서도 첨부파일이 보이게 함

--- components/FeedList/FeedArticle.js
+++ components/FeedList/FeedArticle.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import moment from 'moment';
 
-import {DelEditBox, ArticleTagBox, ArticleCommentBox} from '../boxes';
+import {FileBox, DelEditBox, ArticleTagBox, ArticleCommentBox} from '../boxes';
 
 const FeedArticle = React.createClass({
   render() {
@@ -28,6 +28,7 @@ const FeedArticle = React.createClass({
         <h3 className="article-title">Title: {article.title} Profile: {article.profiles[0].name}</h3>
         <h4 className="article-author">writer: {article.writer.username}</h4>
         <h4 className="article-date"> date: {date}</h4>
+        <FileBox files={article.files}/>
         <div className="article-content">
           {result}
         </div>
