ada6f60e7e23cf8f83ecee4afec562f3e17575a6
Leeingnyo <dyd1928@naver.com>
2016-12-17 16:11:06 +0900

    추천하면 추천 수 오르게 디스패쳐 만듬

--- actions/dispatchers/recommendDispatchers.js
+++ actions/dispatchers/recommendDispatchers.js
@@ -1,19 +1,18 @@
 import {DataCon, Url} from '../../utils';
 import * as types from '../actionTypes';
+import {updateSingleFeed} from '.';
 
 export function recommendArticle(dispatch, id) {
   DataCon.postDataToServer(Url.getUrl(`/articles/${id}/recommend`), 'POST').then(article => {
-    dispatch({
-      type: types.TODO,
-      article
-    });
+    updateSingleFeed(dispatch, {...article, ...{type: 'article'}});
   }).catch(console.error);
 }
 
 export function recommendComment(dispatch, id) {
   DataCon.postDataToServer(Url.getUrl(`/comments/${id}/recommend`), 'POST').then(comment => {
     dispatch({
-      type: types.TODO,
+      type: types.EDIT_COMMENT,
+      articleId: comment.articleId,
       comment
     });
   }).catch(console.error);
@@ -22,7 +21,8 @@ export function recommendComment(dispatch, id) {
 export function recommendProfileComment(dispatch, id) {
   DataCon.postDataToServer(Url.getUrl(`/profile_comments/${id}/recommend`), 'POST').then(comment => {
     dispatch({
-      type: types.TODO,
+      type: types.EDIT_PROFILE_COMMENT,
+      profileId: comment.profileId,
       comment
     });
   }).catch(console.error);
