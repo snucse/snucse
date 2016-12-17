fbd2469d089b3e6cdcb98e3bce7a60bfae3b5014
Leeingnyo <dyd1928@naver.com>
2016-12-18 01:47:30 +0900

    fix

--- actions/dispatchers/recommendDispatchers.js
+++ actions/dispatchers/recommendDispatchers.js
@@ -4,7 +4,7 @@ import {updateSingleFeed} from '.';
 
 export function recommendArticle(dispatch, id) {
   DataCon.postDataToServer(Url.getUrl(`/articles/${id}/recommend`), 'POST').then(article => {
-    updateSingleFeed(dispatch, {...article, ...{type: 'article'}});
+    updateSingleFeed(dispatch, {...article, type: 'article'});
   }).catch(console.error);
 }
 
