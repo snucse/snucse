6b751eff6af236ffe46372082a1648de46f09c16
Sanghyeok Park <pps0209@gmail.com>
2016-12-18 16:08:44 +0900

    utils/DataCon.js 리팩토링

--- utils/DataCon.js
+++ utils/DataCon.js
@@ -1,15 +1,32 @@
 import 'whatwg-fetch';
 
+function fetchHelper(url, options) {
+  const token = localStorage.getItem('snucsesession');
+  options.headers.Authorization = `Token token=${token}`;
+
+  return fetch(url, options).then(res => {
+    if (!res.ok) {
+      throw res;
+    }
+    if (res.status === 204) {
+      return res.text();
+    }
+    return res.json();
+  }).catch(err => {
+    if (err.status === 401) {
+      location.href = '/login';
+    }
+    throw err;
+  });
+}
+
 const DataCon = {
   loadDataFromServer(url) {
     return DataCon.postDataToServer(url, 'GET');
   },
 
   postDataToServer(url, method, data) {
-    const token = localStorage.getItem('snucsesession');
-    const headers = {
-      Authorization: `Token token=${token}`
-    };
+    const headers = {};
     if (data != null) {
       headers['Content-Type'] = 'application/json';
     }
@@ -20,28 +37,12 @@ const DataCon = {
     if (data != null) {
       options.body = JSON.stringify(data);
     }
-    return fetch(url, options).then(res => {
-      if (!res.ok) {
-        throw res;
-      }
-      if (res.status === 204) {
-        return res.text();
-      }
-      return res.json();
-    }).catch(err => {
-      if (err.status === 401) {
-        location.href = '/login';
-      }
-      throw err;
-    });
+
+    return fetchHelper(url, options);
   },
 
   postFormDataToServer(url, method, data) {
-    const token = localStorage.getItem('snucsesession');
-    const headers = {
-      Authorization: `Token token=${token}`
-    };
-
+    const headers = {};
     const formData = new FormData();
     for (const key in data) {
       if (Object.hasOwnProperty.call(data, key)) {
@@ -60,20 +61,8 @@ const DataCon = {
       headers,
       body: formData
     };
-    return fetch(url, options).then(res => {
-      if (!res.ok) {
-        throw res;
-      }
-      if (res.status === 204) {
-        return res.text();
-      }
-      return res.json();
-    }).catch(err => {
-      if (err.status === 401) {
-        location.href = '/login';
-      }
-      throw err;
-    });
+
+    return fetchHelper(url, options);
   }
 };
 
