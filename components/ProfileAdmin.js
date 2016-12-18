721942a1f8db16bbe03179dc403e98e0ab939db3
Sanghyeok Park <pps0209@gmail.com>
2016-12-18 16:38:55 +0900

    프로필 설정 페이지 분리

--- components/ProfileAdmin.js
+++ components/ProfileAdmin.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import {ProfileAdminBox} from './boxes/ProfileBox';
+import {ProfileBox} from './boxes/ProfileBox';
 
 const ProfileAdmin = React.createClass({
   render() {
     const {id} = this.props.params;
-    return <ProfileAdminBox id={id}/>;
+    return <ProfileBox id={id}/>;
   }
 });
 
