06bf3f40e60e40133f507ec3a115f9b6e2a8bd2c
Sanghyeok Park <pps0209@gmail.com>
2016-12-18 20:24:07 +0900

    가입 신청 구현

--- components/SignUp.js
+++ components/SignUp.js
@@ -13,20 +13,22 @@ const SignUp = React.createClass({
   }
 });
 
-const formNames = ['id', 'password', 'password2', 'birthYear', 'birthMonth', 'birthDay', 'bsNumber1', 'bsNnumber2', 'phoneNumber1', 'phoneNumber2', 'phoneNumber3'];
+const formNames = ['username', 'password', 'password2', 'name', 'birthday', 'bsNumber', 'phoneNumber'];
 const birthReg = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/;
 const bsNumReg = /^[0-9]{4}-[0-9]{5}$/;
 const phoneNumReg = /^[0-9]{3}-[0-9]{3,4}-[0-9]{4}$/;
 
 const SignUpForm = React.createClass({
-  handleSignUp() {
+  handleSignUp(e) {
+    e.preventDefault();
     const values = this.validateForm();
-    if (!values) {
+    if (values === null) {
       return;
     }
 
     DataCon.postDataToServer(Url.getUrl('/users/sign_up'), 'POST', values)
       .then(() => {
+        alert('가입에 성공하였습니다.');
         browserHistory.push('/login');
       }).catch(() => {
         // TODO: 에러 발생 조건에 뭐가 있는지 확인하기
@@ -34,8 +36,8 @@ const SignUpForm = React.createClass({
       });
   },
 
-  renderInput(refName, type = 'text') {
-    return <input type={type} ref={genRefCallback(this, refName)}/>;
+  renderInput(refName, placeholder, type = 'text') {
+    return <input type={type} placeholder={placeholder} ref={genRefCallback(this, refName)}/>;
   },
 
   validateForm() {
@@ -43,25 +45,37 @@ const SignUpForm = React.createClass({
       return {...prev, [curr]: this[curr].value};
     }, {});
 
+    if (values.username.length === 0) {
+      alert('아이디를 입력해주세요.');
+      return null;
+    }
+
+    if (values.password.length === 0) {
+      alert('비밀번호를 입력해주세요.');
+      return null;
+    }
+
     if (values.password !== values.password2) {
       alert('비밀번호를 확인해주세요.');
       return null;
     }
 
-    const birth = [values.birthYear, values.birthMonth, values.birthDay].join('-');
-    if (birth !== '--' && !(birthReg.test(birth))) {
+    if (values.name.length === 0) {
+      alert('이름을 입력해주세요.');
+      return null;
+    }
+
+    if (!(birthReg.test(values.birthday))) {
       alert('생년월일을 정확히 입력해주세요. e.g.) 1900-01-01');
       return null;
     }
 
-    const bsNumber = [values.bsNumber1, values.bsNumber2].join('-');
-    if (bsNumber !== '-' && !(bsNumReg.test(bsNumber))) {
+    if (!(bsNumReg.test(values.bsNumber))) {
       alert('학번을 정확히 입력해주세요. e.g.) 2017-10000');
       return null;
     }
 
-    const phoneNumber = [values.phoneNumber1, values.phoneNumber2, values.phoneNumber3].join('-');
-    if (phoneNumber !== '--' && !(phoneNumReg.test(phoneNumber))) {
+    if (!(phoneNumReg.test(values.phoneNumber))) {
       alert('전화번호를 정확히 입력해주세요. e.g.) 010-123-4567 또는 010-1234-5678');
       return null;
     }
@@ -70,18 +84,16 @@ const SignUpForm = React.createClass({
   },
 
   render() {
-    // 생년월일: {this.renderInput('birthYear')}년 {this.renderInput('birthMonth')}월 {this.renderInput('birthDay')}일<br/>
     return (
       <form>
         가입<br/>
-        * 표시는 필수사항입니다.<br/>
-        * 아이디: <input type="text" autoFocus ref={genRefCallback(this, 'id')}/><br/>
-        * 비밀번호: {this.renderInput('password')}<br/>
-        * 비밀번호 확인: {this.renderInput('password2', 'password')}<br/>
-        * 이름: {this.renderInput('myname')}<br/>
-        생년월일: {this.renderInput('birth', 'date')}<br/>
-        학번: {this.renderInput('bsNumber1')}-{this.renderInput('bsNumber2')}<br/>
-        휴대전화: {this.renderInput('phoneNumber1')}-{this.renderInput('phoneNumber2')}-{this.renderInput('phoneNumber3')}<br/>
+        아이디: {this.renderInput('username', 'id')}<br/>
+        비밀번호: {this.renderInput('password', '******', 'password')}<br/>
+        비밀번호 확인: {this.renderInput('password2', '******', 'password')}<br/>
+        이름: {this.renderInput('name', '홍길동')}<br/>
+        생년월일: {this.renderInput('birthday', '1900-01-01', 'date')}<br/>
+        학번: {this.renderInput('bsNumber', '2017-10000')}<br/>
+        휴대전화: {this.renderInput('phoneNumber', '010-1234-5678')}<br/>
         <button onClick={this.handleSignUp}>가입</button>
       </form>
     );
