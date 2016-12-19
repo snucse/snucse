f018c2fa0678eb0ca37a4a4e38ceb25f7cc6099c
Leeingnyo <dyd1928@naver.com>
2016-12-19 21:05:34 +0900

    apply SearchForm

--- components/boxes/SearchBox/SearchForm.js
+++ components/boxes/SearchBox/SearchForm.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import {browserHistory} from 'react-router';
 
 import {genRefCallback} from '../../../utils';
+import {alertModal} from '../../../actions/dispatchers';
 
-export default React.createClass({
+const SearchForm = React.createClass({
   handleSubmit(event) {
     event.preventDefault();
     // this.props.onSearch(this._content.value);
@@ -16,7 +18,7 @@ export default React.createClass({
     */
     const query = this._content.value;
     if (query.length < 2) {
-      alert('두 글자 이상 입력해주세요');
+      this.props.alertModal('알림', '두 글자 이상 입력해주세요');
       return;
     }
     browserHistory.push(`/search?query=${query}`);
@@ -33,3 +35,11 @@ export default React.createClass({
     );
   }
 });
+
+const mapDispatchToProps = function (dispatch) {
+  return {
+    alertModal: (title, message, callback) => alertModal(dispatch, title, message, callback)
+  };
+};
+
+export default connect(null, mapDispatchToProps)(SearchForm);
