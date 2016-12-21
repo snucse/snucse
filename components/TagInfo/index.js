ce207568cf637d8e353ad40455c69eb6f2a8fa5b
Sanghyeok Park <pps0209@gmail.com>
2016-12-21 11:27:35 +0900

    /tags에 쿼리 없이 접근 할 경우 예외처리

--- components/TagInfo/index.js
+++ components/TagInfo/index.js
@@ -4,6 +4,10 @@ import TagContainer from './TagContainer';
 
 const TagInfo = React.createClass({
   render() {
+    if (!(this.props.location.query && this.props.location.query.tag)) {
+      return <p>정상적이지 않은 접근입니다.</p>;
+    }
+
     return <TagContainer tagName={this.props.location.query.tag}/>;
   }
 });
