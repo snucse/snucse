dea192e0037886c0547f5c0677bde7dcfcacf286
Sanghyeok Park <pps0209@gmail.com>
2016-12-21 13:34:43 +0900

    /tags 경로에 접근 했을 때 modal이 뜨게 함

--- components/TagInfo/index.js
+++ components/TagInfo/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import {browserHistory} from 'react-router';
 
+import {connectModals} from '../../utils';
 import TagContainer from './TagContainer';
 
 const TagInfo = React.createClass({
   render() {
     if (!(this.props.location.query && this.props.location.query.tag)) {
-      return <p>정상적이지 않은 접근입니다.</p>;
+      this.props.alertModal('알림', '정상적이지 않은 접근입니다.', () => {
+        browserHistory.goBack();
+      });
+      return null;
     }
 
     return <TagContainer tagName={this.props.location.query.tag}/>;
   }
 });
 
-export default TagInfo;
+export default connectModals(TagInfo);
