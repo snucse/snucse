0e50f793cdab3298ad4d969ac02d1972f0c87358
Leeingnyo <dyd1928@naver.com>
2017-02-23 21:56:53 +0900

    implement components related FollowingProfile

--- components/FollowingProfileBox/FollowingProfileBox.js
+++ components/FollowingProfileBox/FollowingProfileBox.js
@@ -1,6 +1,83 @@
 import React from 'react';
 import {Link} from 'react-router';
 import {connect} from 'react-redux';
+import classnames from 'classnames';
+
+const FollowingProfileListItem = React.createClass({
+
+  propTypes: {
+    onClickSetTab: React.PropTypes.func,
+    onClickStar: React.PropTypes.func,
+    profile: React.PropTypes.object
+  },
+
+  handleClickStar() {
+    this.props.onClickStar();
+  },
+
+  handleClickSetTab() {
+    this.props.onClickSetTab();
+  },
+
+  render() {
+    const {profile} = this.props;
+    const starClass = classnames({
+      star: profile.star
+    });
+    const starView = (
+      <span onClick={this.handleClickStar} className={starClass}>
+        {profile.star ? '★' : '☆'}
+      </span>
+    );
+    const tabClass = classnames({
+      tabbed: profile.tab
+    });
+    const setTabView = profile.star ? (
+      <span onClick={this.handleClickSetTab} className={tabClass}>
+        {profile.tab ? '◎' : '○'}
+      </span>
+    ) : null;
+    return (
+      <li className="following-profiles-list-item">
+        {starView}
+        {setTabView}
+        <Link to={`/${profile.id}`}>{profile.name}</Link>
+      </li>
+    );
+  }
+});
+
+const FollowingProfileListItemContainer = React.createClass({
+
+  propTypes: {
+    onClickSetTab: React.PropTypes.func,
+    onClickStar: React.PropTypes.func,
+    profile: React.PropTypes.object
+  },
+
+  render() {
+    return (
+      <FollowingProfileListItem
+        onClickSetTab={this.props.onClickSetTab}
+        onClickStar={this.props.onClickStar}
+        profile={this.props.profile}
+        />
+    );
+  }
+});
+// container
+// profile 받음
+// star 판단
+  // 눌렀을 때 다른 일 하도록 부여
+// tag 판단
+  // 눌렀을 때 다른 일 하도록 부여
+// item에게 줌
+
+/*
+const Normal FollowingProfileListItemContainer
+const Star FollowingProfileListItemContainer
+const Tab FollowingProfileListItemContainer
+*/
 
 const FollowingProfileBox = React.createClass({
 
@@ -11,7 +88,7 @@ const FollowingProfileBox = React.createClass({
   render() {
     const profiles = this.props.me.following.map(profile => {
       return (
-        <li key={`${profile.id}${profile.name}`}><Link to={`/${profile.id}`}>{profile.name}</Link></li>
+        <FollowingProfileListItemContainer key={`${profile.id}${profile.name}`} profile={profile}/>
       );
     });
     const allProfileLink = this.props.showAllProfileLink ?
