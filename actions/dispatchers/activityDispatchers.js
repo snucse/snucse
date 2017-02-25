ecddf2ab79ef5f86d432091b58392559c721d61a
Leeingnyo <dyd1928@naver.com>
2017-02-25 17:36:06 +0900

    define actions, dispatcher

---
+++ actions/dispatchers/activityDispatchers.js
@@ -0,0 +1,24 @@
+import {DataCon, Url} from '../../utils';
+import * as types from '../actionTypes';
+
+export function loadActivity(dispatch, options) {
+  dispatch({
+    type: types.LOAD_ACTIVITY
+  });
+  // options: {profileId, filterType, filterAction, page, limit}
+  // todo options 제약을 여기서 걸어야 하나?
+  const page = options.page || 0;
+  options.page = Number(page) + 1;
+  DataCon.loadDataFromServer(Url.getUrl('/activities', options)).then(response => {
+    dispatch({
+      type: types.LOAD_ACTIVITY_SUCCESS,
+      activities: response.activities,
+      count: response.count
+    });
+  }).catch(err => {
+    console.error(err);
+    dispatch({
+      type: types.LOAD_ACTIVITY_ERROR
+    });
+  });
+}
