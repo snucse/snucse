3a63b6d1ddb834cc6012495c2c9aa2e99ebc9b63
Leeingnyo <dyd1928@naver.com>
2017-03-01 04:20:02 +0900

    make reducer

--- reducers/activity.js
+++ reducers/activity.js
@@ -1,14 +1,19 @@
 import {
   LOAD_ACTIVITY,
   LOAD_ACTIVITY_SUCCESS,
-  LOAD_ACTIVITY_ERROR
+  LOAD_ACTIVITY_ERROR,
+  SEARCH_PROFILE_WITH_PREFIX_INIT,
+  SEARCH_PROFILE_WITH_PREFIX_COMPLETE
 } from '../actions/actionTypes';
 import {updateObject, createReducer} from './common';
 
 const ACTIVITY_INITIAL_STATE = {
   activities: [],
   loading: false,
-  isError: false
+  isError: false,
+  autoComplete: {
+    profiles: []
+  }
 };
 
 function load(state) {
@@ -24,8 +29,28 @@ function fail(state) {
   return updateObject(state, {loading: false, isError: true});
 }
 
+function searchProfileWithPrefixInit(state) {
+  const autoComplete = updateObject(state.autoComplete, {
+    profiles: []
+  });
+  return updateObject(state, {
+    autoComplete
+  });
+}
+// fixme you can extract this like tag dispatcher
+function searchProfileWithPrefixComplete(state, action) {
+  const autoComplete = updateObject(state.autoComplete, {
+    profiles: action.profiles
+  });
+  return updateObject(state, {
+    autoComplete
+  });
+}
+
 export default createReducer(ACTIVITY_INITIAL_STATE, {
   [LOAD_ACTIVITY]: load,
   [LOAD_ACTIVITY_SUCCESS]: success,
-  [LOAD_ACTIVITY_ERROR]: fail
+  [LOAD_ACTIVITY_ERROR]: fail,
+  [SEARCH_PROFILE_WITH_PREFIX_INIT]: searchProfileWithPrefixInit,
+  [SEARCH_PROFILE_WITH_PREFIX_COMPLETE]: searchProfileWithPrefixComplete
 });
